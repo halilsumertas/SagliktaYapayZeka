8e65d4aaab6408a4d273acf30be6fb64ca11bb0c
Halil Sümertaş <halilsumertas11@gmail.com>
2021-06-27 21:23:26 +0300

    Döngü güncellemesi
    While döngüsü üzerinde değişiklikler yapıldı

--- 1. Ödev/tansiyonvki.py
+++ 1. Ödev/tansiyonvki.py
@@ -29,39 +29,39 @@ def TansiyonHesap(sistolik,diastolik):
         tansiyon = "İzole sistolik hipertansiyon"
         return tansiyon
 #Program boyunca kullanacağımız değişkenler
-dongu = True #Programı sürdürmek için kullanacağımız değişken
 Kayitlar = [] #Kayıtları tutacağımız değişken
 
 #Arayüz
 print("Kan Basıncı ve Vücut Kitle Endeksi Değerlendirme Uygulmasına Hoşgeldiniz! \n")
-while dongu:
-    print("Lütfen yapmak istediğiniz işlemi seçiniz. \n")
-    secim = int(input("Kan Basıncı değerlendirmesi için 1 \nVücut Kitle Endeksi hesaplaması için 2 \nGeçmiş kayıtları görüntülemek için 3 \nUygulamadan çıkmak için 0 \n"))
-    if secim == 1:
-        print("Kan Basıncı değerlendirme uygulması. \n")
-        ad = input("Lütfen adınızı giriniz: ")
-        soyad = input("Lütfen soyadınızı giriniz: ")
-        sistolik = int(input("Lütfen sistolik kan basıncınızı giriniz: "))
-        diastolik = int(input("Lütfen diastolik kan basıncınızı giriniz: "))
-        hasta_tansiyon = TansiyonHesap(sistolik,diastolik)
-        hasta_kayit = [ad, soyad, hasta_tansiyon]
-        Kayitlar = Kayitlar + hasta_kayit
-        print(f"Sayın {ad} {soyad}. Teşhisiniz: {hasta_tansiyon}. Detaylı bilgi için bağlantıyı takip edebilirisiniz: https://tkd.org.tr/kilavuz/k03/3_2d304.htm?wbnum=1104 \n")
-    elif secim == 2:
-        print("Vücut Kitle Endeksi hesaplama uygulaması.")
-        ad = input("Lütfen adınızı giriniz: ")
-        soyad = input("Lütfen soyadınızı giriniz: ")
-        boy = int(input("Lütfen boyunuzu cm cinsinden giriniz: "))
-        kilo = int(input("Lütfen kilonuzu kg cinsinden giriniz: "))
-        hasta_vki = VkiHesap(boy,kilo)
-        hasta_kayit = [ad, soyad, hasta_vki]
-        Kayitlar = Kayitlar + hasta_kayit
-        print(f"Sayın {ad} {soyad}. Vücut Kitle Endeksiniz: {hasta_vki}. ")
-    elif secim == 3:
-        print(Kayitlar)
-    elif secim == 0:
-        dongu = False
-    else:
-        print("Hatalı seçim yaptınız!")
-
-
+while True:
+    try:
+        print("Lütfen yapmak istediğiniz işlemi seçiniz. \n")
+        secim = int(input("Kan Basıncı değerlendirmesi için 1 \nVücut Kitle Endeksi hesaplaması için 2 \nGeçmiş kayıtları görüntülemek için 3 \nUygulamadan çıkmak için 0 \n"))
+        if secim == 1:
+            print("Kan Basıncı değerlendirme uygulması. \n")
+            ad = input("Lütfen adınızı giriniz: ")
+            soyad = input("Lütfen soyadınızı giriniz: ")
+            sistolik = int(input("Lütfen sistolik kan basıncınızı giriniz: "))
+            diastolik = int(input("Lütfen diastolik kan basıncınızı giriniz: "))
+            hasta_tansiyon = TansiyonHesap(sistolik,diastolik)
+            hasta_kayit = [ad, soyad, hasta_tansiyon]
+            Kayitlar = Kayitlar + hasta_kayit
+            print(f"Sayın {ad} {soyad}. Teşhisiniz: {hasta_tansiyon}. Detaylı bilgi için bağlantıyı takip edebilirisiniz: https://tkd.org.tr/kilavuz/k03/3_2d304.htm?wbnum=1104 \n")
+        elif secim == 2:
+            print("Vücut Kitle Endeksi hesaplama uygulaması.")
+            ad = input("Lütfen adınızı giriniz: ")
+            soyad = input("Lütfen soyadınızı giriniz: ")
+            boy = int(input("Lütfen boyunuzu cm cinsinden giriniz: "))
+            kilo = int(input("Lütfen kilonuzu kg cinsinden giriniz: "))
+            hasta_vki = VkiHesap(boy,kilo)
+            hasta_kayit = [ad, soyad, hasta_vki]
+            Kayitlar = Kayitlar + hasta_kayit
+            print(f"Sayın {ad} {soyad}. Vücut Kitle Endeksiniz: {hasta_vki}. ")
+        elif secim == 3:
+            print(Kayitlar)
+        elif secim == 0:
+            break
+        else:
+            print("Hatalı seçim yaptınız!")
+    except:
+        print("Hatalı seçim yaptınızz!")
